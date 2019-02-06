368e93ff08e518f6b5290e30053e844c9c66fb54
CarlosOrtiz9901 <carloslosada9901@gmail.com>
2019-02-05 21:54:33 -0500

    Update PRODUCTOS.cs

--- Proyecto_Web/Proyecto_Web/Modelos/PRODUCTOS.cs
+++ Proyecto_Web/Proyecto_Web/Modelos/PRODUCTOS.cs
@@ -11,6 +11,7 @@ namespace Proyecto_Web.Modelos
 {
     public class PRODUCTOS
     {
+        private IDatos dat = new Datos();
         int ID_PRODCUTOS { get; set; }
         string PRO_NOMBRE { get; set; }
         float PRO_CANTIDAD { get; set; }
@@ -29,6 +30,41 @@ namespace Proyecto_Web.Modelos
             PRO_NOMBRE = nom;
             PRO_CANTIDAD = cantidad;
             PRO_PRECIO = precio;
+        }
+        public bool RegistrarProducto(string NOM, float cant, double precio)
+        {
+            try
+            {
+                return dat.OperarDatos("CALL INSE_PRODUCTO ('" + NOM + "', '" + cant + "', '" + precio + "');");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public DataTable ConsultarProducto(string ID)
+        {
+            try
+            {
+                return dat.ConsultarDatos("CALL CONS_PRODUCTO ('" + ID + "');");
+            }
+            catch (Exception io)
+            {
+                estructura err = new estructura();
+                return err.GetError(io.Message);
+            }
+        }
+        public bool ActualizarProducto(int id, string nom, string ubi, string dime)
+        {
+            try
+            {
+                return dat.OperarDatos("CALL UPDA_PRODUCTO ('" + id + "', '" + nom + "', '" + ubi + "', '" + dime + "');");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
     }
 }
