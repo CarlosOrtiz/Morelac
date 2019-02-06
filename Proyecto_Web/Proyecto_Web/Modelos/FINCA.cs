5fd2cfca43c6f7e52570bc4418cd8f5e081a30e1
CarlosOrtiz9901 <carloslosada9901@gmail.com>
2019-02-05 19:16:40 -0500

    Update FINCA.cs

--- Proyecto_Web/Proyecto_Web/Modelos/FINCA.cs
+++ Proyecto_Web/Proyecto_Web/Modelos/FINCA.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Proyecto_Web.Conection;
+using Proyecto_Web.Interface;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +10,7 @@ namespace Proyecto_Web.Modelos
 {
     public class FINCA
     {
+        private IDatos dat = new Datos();
         string FIN_NOMBRE { get; set; }
         string FIN_UBICACION { get; set; }
         string FIN_DIMENCION { get; set; }
@@ -25,6 +29,40 @@ namespace Proyecto_Web.Modelos
             FIN_UBICACION = ubicacion;
 
         }
+        public bool RegistrarFinca(string NOM, string DIM, string UBI, string ID_PERSONA)
+        {
+            try
+            {
+                return dat.OperarDatos("CALL INSE_FINCA ('" +NOM  + "', '" + DIM + "', '" + UBI + "', '" + ID_PERSONA + "');");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public DataTable ConsultarFinca(string Cedula)
+        {
+            try
+            {
+                return dat.ConsultarDatos("CALL CONS_PERSONA ('" + Cedula + "');");
+            }
+            catch (Exception io)
+            {
+                estructura err = new estructura();
+                return err.GetError(io.Message);
+            }
+        }
+        public bool Actualizar(int id, string nom, string ape, string naci, char sexo, string num, string bio, string foto, string con, string estado)
+        {
+            try
+            {
+                return dat.OperarDatos("CALL UPDA_PERSONA ('" + id + "', '" + nom + "', '" + ape + "', '" + naci + "', '" + sexo + "', '" + num + "', '" + bio + "', '" + foto + "','" + con + "','" + estado + "');");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
 
+        }
     }
 }
