9ec356de146f191b83fa3f9afd299a71ff166739
JohantVargas <JohantVargas@users.noreply.github.com>
2019-02-26 15:51:49 -0500

    CAMBIO REPORTE
    YA FUNCIONA

--- Proyecto_Web/Proyecto_Web/Vistas/Private/Reportes/REPORTEEE.aspx.cs
+++ Proyecto_Web/Proyecto_Web/Vistas/Private/Reportes/REPORTEEE.aspx.cs
@@ -15,7 +15,9 @@ namespace Proyecto_Web.Vistas.Private
     public partial class REPORTEEE : System.Web.UI.Page
     {
         private IDatos dat = new Datos();
-        PROVEEDOR m_pro = new PROVEEDOR();
+        DataSet dts = new DataSet();
+        DataTable da, em = new DataTable();
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             try
@@ -30,22 +32,26 @@ namespace Proyecto_Web.Vistas.Private
                 Response.Redirect("~/Vistas/Public/Index.aspx");
             }
 
-            DataSet dts = new DataSet();
-            DataTable da, em = new DataTable();
+            
 
             Lis_proveedores rep = new Lis_proveedores();
-            da = dat.ConsultarDatos("CALL CONS_PROVEEDOR_REPORTE");
+           
+            
+
+            da = dat.ConsultarDatos("SELECT ID_PERSONA,PER_NOMBRE1,PER_NOMBRE2,PER_APELLIDO1,PER_APELLIDO2,PER_DIRECCION, USU_ESTADO AS ESTADO FROM persona INNER JOIN usuario ON(persona.ID_PERSONA = usuario.PERSONA_ID_PERSONA) INNER JOIN rol ON(usuario.ROL_ID_ROL = rol.ID_ROL) WHERE rol.ID_ROL = 5 AND usuario.USU_ESTADO = 'ACTIVO' ; ");
+           
+            
             rep.SetDataSource(da);
             
+
 
             em = dat.ConsultarDatos("Select EMP_NOMBRE From empresa ;");
             rep.SetParameterValue("Empresa", em.Rows[0]["EMP_NOMBRE"].ToString());
 
             re_proveedoores.ReportSource = rep;
-            //re_proveedoores.Height = 200;
-            //re_proveedoores.Width = 400;
-            re_proveedoores.DataBind();
-
+            re_proveedoores.Height = 200;
+            re_proveedoores.Width = 400;
+            //re_proveedoores.RefreshReport();
         }
 
      
