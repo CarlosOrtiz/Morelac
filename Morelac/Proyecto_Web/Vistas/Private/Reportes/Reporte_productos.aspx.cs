ea7e0fe21f7c000fd3e2f3bbf48b5065cc6162e7
JohantVargas <JohantVargas@users.noreply.github.com>
2019-03-02 19:44:16 -0500

    REPORTE PRODUCTOS

--- Morelac/Proyecto_Web/Vistas/Private/Reportes/Reporte_productos.aspx.cs
+++ Morelac/Proyecto_Web/Vistas/Private/Reportes/Reporte_productos.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using Proyecto_Web.Conection;
 using Proyecto_Web.Interface;
+using Proyecto_Web.Reportes;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -7,12 +8,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Proyecto_Web.Modelos;
-using Proyecto_Web.Reportes;
 
-namespace Proyecto_Web.Vistas.Private
+namespace Proyecto_Web.Vistas.Private.Reportes
 {
-    public partial class REPORTEEE : System.Web.UI.Page
+    public partial class Reporte_productos : System.Web.UI.Page
     {
         private IDatos dat = new Datos();
         DataSet dts = new DataSet();
@@ -30,14 +29,14 @@ namespace Proyecto_Web.Vistas.Private
             {
                 Response.Redirect("~/Vistas/Public/Index.aspx");
             }
-            Lis_proveedores rep = new Lis_proveedores();
-            da = dat.ConsultarDatos("SELECT ID_PERSONA,PER_NOMBRE1,PER_NOMBRE2,PER_APELLIDO1,PER_APELLIDO2,PER_DIRECCION, USU_ESTADO AS ESTADO FROM persona INNER JOIN usuario ON(persona.ID_PERSONA = usuario.PERSONA_ID_PERSONA) INNER JOIN rol ON(usuario.ROL_ID_ROL = rol.ID_ROL) WHERE rol.ID_ROL = 5 AND usuario.USU_ESTADO = 'ACTIVO' ; ");
+            Lis_productos rep = new Lis_productos();
+            da = dat.ConsultarDatos("CALL CONS_PRODUCTOS_REPORTE;");
             rep.SetDataSource(da);
             em = dat.ConsultarDatos("Select EMP_NOMBRE From empresa ;");
             rep.SetParameterValue("Empresa", em.Rows[0]["EMP_NOMBRE"].ToString());
-            re_proveedoores.ReportSource = rep;
-            re_proveedoores.Height = 200;
-            re_proveedoores.Width = 400;
+            re_productos.ReportSource = rep;
+            re_productos.Height = 200;
+            re_productos.Width = 400;
             //re_proveedoores.RefreshReport();
         }
     }
